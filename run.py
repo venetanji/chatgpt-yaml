98a0d893bb65f726d286abedfc7aed0d0d0d6d4d
Giovanni Lion <giovanni.lion@gmail.com>
2023-03-10 14:39:36 +0700

    initial commit

---
+++ run.py
@@ -0,0 +1,29 @@
+# import python yaml module
+import yaml
+from dotenv import load_dotenv
+import urllib.request
+import os
+import openai
+import logging
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+# load config.yaml
+config = yaml.safe_load(open('example.yaml','r'))
+print(config)
+
+
+class Template:
+
+    def __init__(self, template="example"):
+        self.config = yaml.safe_load(open(f'{template}.yaml','r'))
+        self.completion = None
+
+    def run(self):
+        self.completion = openai.ChatCompletion.create(**config)
+        logging.debug("Completion: "+ str(self.completion))
+        return self.completion.choices[0]['message']['content']
+
+template = Template()
+print(template.run())
